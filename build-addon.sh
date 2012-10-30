79fc4ea850623894299d8a4da26e4e9ccc7a30f8
xanda <adnan.shukor@gmail.com>
2012-10-30 11:53:35 +0800

    Mozilla addon builder

---
+++ build-addon.sh
@@ -0,0 +1,21 @@
+BUILDnum=`head -n 1 dictionaries/en-MY.dic`
+cat Mozilla-addon/install.rdf.orig | sed 's/\[build\_number\]/'$BUILDnum'/g' > Mozilla-addon/install.rdf
+
+cp dictionaries/en-MY.* Mozilla-addon/dictionaries/
+cd Mozilla-addon
+
+whichZIP=`which zip`
+
+if [ "$whichZIP" != "/usr/bin/zip" ];
+then
+   echo "===============ERROR================"
+   echo " ERROR! zip is not installed"
+   echo "===============DONE=================" && exit 1
+fi
+
+zip ../en-MY-Rojak.xpi dictionaries/* install.rdf icon.png
+rm dictionaries/*
+rm install.rdf
+
+echo ""
+echo "en-MY-Rojak.xpi created!"
