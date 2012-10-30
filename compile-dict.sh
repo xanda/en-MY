0a99f2df2ed408f2c1c97c5e8c92af633e9c2b9a
xanda <adnan.shukor@gmail.com>
2012-10-30 12:48:54 +0800

    Sort first in tmp file

--- compile-dict.sh
+++ compile-dict.sh
@@ -1,3 +1,5 @@
+grep -v '^ *$' en-MY.txt | sort | uniq > en-MY.tmp
+mv en-MY.tmp en-MY.txt
 grep -v '^ *$' en-MY.txt | sort | uniq | wc -l | grep -E -o '[0-9]{1,}' > dictionaries/en-MY.dic
 echo "" >> dictionaries/en-MY.dic
 grep -v '^ *$' en-MY.txt | sort | uniq >> dictionaries/en-MY.dic
